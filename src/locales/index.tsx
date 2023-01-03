00fea617b4ca88866a720ddbeeeef1b08ed89bd8
Shohei Okubo <stream.larn3@gmail.com>
2023-01-04 03:42:14 +0900

    lf化

--- src/locales/index.tsx
+++ src/locales/index.tsx
@@ -1,11 +1,12 @@
 import { I18nContext, createI18nContext } from '@solid-primitives/i18n'
 import type { FlowComponent } from 'solid-js'
+import ja from './ja'
 import en from './en'
 import zhCN from './zh-CN'
 
-const ctx = createI18nContext({ en, zhCN }, 'en')
+const ctx = createI18nContext({ ja, en, zhCN }, 'ja')
 
-const availableLocales = ['en', 'zhCN']
+const availableLocales = ['ja', 'en', 'zhCN']
 
 const I18nProvider: FlowComponent<{
   dict?: Record<string, Record<string, any>>
@@ -14,4 +15,4 @@ const I18nProvider: FlowComponent<{
   return <I18nContext.Provider value={ctx}>{props.children}</I18nContext.Provider>
 }
 
-export { en, zhCN, I18nProvider, availableLocales }
+export { ja, en, zhCN, I18nProvider, availableLocales }
---
+++ src/locales/ja.tsx
@@ -0,0 +1,16 @@
+const ja = {
+  btn_about: 'About',
+  btn_back: 'Back',
+  btn_go: 'GO',
+  btn_home: 'Home',
+  btn_toggle_dark: 'ダークモード切り替え',
+  btn_toggle_langs: '言語を変更する',
+  intro_desc: '熟考されたVite スターターテンプレート',
+  intro_dynamic_route: '動的ルートのテスト',
+  intro_hi: 'やあ、 {{ name }}！',
+  intro_aka: 'a.k.a.',
+  intro_whats_your_name: 'あなたの名前は？',
+  not_found: '見つかりません',
+}
+
+export default ja
